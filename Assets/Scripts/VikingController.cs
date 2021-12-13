41cd884da47dfa7e338f9aa1af3dfd931ce4fc7e
CaramelYoPC <alltime326@gmail.com>
2021-12-14 03:27:12 +0800

    complete animator part and modify some part of navigation

--- Assets/Scripts/VikingController.cs
+++ Assets/Scripts/VikingController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-[RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class VikingController : MonoBehaviour
 {
     public Vector3 MovingDirection;
-    public float JumpingForce;
+    public float JumpingForce, MovingThreshold;
 
     //MeshRenderer mr;
     Rigidbody rb;
-    [SerializeField]float movingSpeed = 10f;
-    bool onGround = false;
-
+    Animator animator;
     NavMeshAgent agent;
+
+    // a structure used to get information back from a raycast
+    RaycastHit raycastHit;
+    //Vector2 velocity = Vector2.zero;
+    [SerializeField]float movingSpeed = 10f;
+    bool onGround = false, run = false;
 
 
     void Awake()
@@ -29,8 +33,9 @@ public class VikingController : MonoBehaviour
     {
         //mr = GetComponent<MeshRenderer>();
         rb = GetComponent<Rigidbody>();
-
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        //agent.updatePosition = false;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -61,23 +66,32 @@ public class VikingController : MonoBehaviour
         //mr.material.color = new Color((int)Time.time % 2 * 1.0f, 1.0f, 1.0f);
 
 
+        // initiate / reset run
+        run = false;
+
         // move by the key
         if (Input.GetKey(KeyCode.W))
         {
             transform.localPosition += movingSpeed * Time.deltaTime * Vector3.forward;
+            run = true;
         }
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.localPosition += movingSpeed * Time.deltaTime * Vector3.back;
+            run = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.localPosition += movingSpeed * Time.deltaTime * Vector3.left;
+            run = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.localPosition += movingSpeed * Time.deltaTime * Vector3.right;
+            run = true;
         }
+
+        animator.SetBool("Run", run);
 
 
         // jump
@@ -87,34 +101,49 @@ public class VikingController : MonoBehaviour
         }
 
 
-        // destroy a coin when it is clicked by mouse
+        //// destroy a coin when it is clicked by mouse
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    // 2D to 3D
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    // a structure used to get information back from a raycast
+        //    RaycastHit raycastHit;
+
+        //    // raycasting
+        //    if (Physics.Raycast(ray, out raycastHit))
+        //    {
+        //        if (raycastHit.transform.name.Contains("Viking_Shield_Coin"))
+        //            Destroy(raycastHit.transform.gameObject);
+        //    }
+        //}
+
+
+        // walk to the position mouse clicked by nav agent
         if (Input.GetMouseButtonDown(0))
         {
-            // 2D to 3D
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // a structure used to get information back from a raycast
-            RaycastHit raycastHit;
-
-            // raycasting
             if (Physics.Raycast(ray, out raycastHit))
             {
-                if (raycastHit.transform.name.Contains("Viking_Shield_Coin"))
-                    Destroy(raycastHit.transform.gameObject);
+                agent.SetDestination(raycastHit.point);
             }
         }
 
+        //// nav agent animation
+        //Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+        //float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        ////float dy = Vector3.Dot(transform.up, worldDeltaPosition);
+        //float dz = Vector3.Dot(transform.forward, worldDeltaPosition);
+        //Vector2 deltaPosition = new Vector2(dx, dz);
+        //velocity = deltaPosition / Time.deltaTime;
+        //run = velocity.magnitude > MovingThreshold && agent.remainingDistance > agent.radius;
 
-        //// walk to the position mouse clicked by nav agent
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //// update animator parameters
+        //animator.SetBool("Run", run);
+    }
 
-        //    RaycastHit raycastHit;
-
-        //    if (Physics.Raycast(ray, out raycastHit))
-        //    {
-        //        agent.SetDestination(raycastHit.point);
-        //    }
-        //}
+    void OnAnimatorMove()
+    {
+        // update position to agent position
+        transform.position = agent.nextPosition;
     }
 }
